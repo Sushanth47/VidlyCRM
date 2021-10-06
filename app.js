a770910abccc8390b2a7e4bc0f8de8bbd597ba68
Sushanth47 <chandupopstar@gmail.com>
2021-10-06 17:56:13 +0530

    Done

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.use(express.static(__dirname + "public"));
 app.use(cookieParser());
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
-  // console.log(req.user, 'requserinappjs')
   next();
 });
 
--- controllers/crmController.js
+++ controllers/crmController.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 var express = require("express");
 const { Movie } = require("../models/movieModel");
-// const Movie = require('../routes/crmRoutes')
+
 exports.getDashboardPage = async (req, res) => {
   return res.status(200).render("dashboard.ejs");
 };
 
 exports.getDashboard = async (req, res) => {
-  var moviesList = await Movie.find({});
+  var moviesList = await Movie.aggregate([
+    {
+      $project: {
+        _id: 1,
+        title: 1,
+        rentedCustomers: 1,
+      },
+    },
+    {
+      $limit: 5,
+    },
+  ]);
   return res.status(200).json(moviesList);
 };
